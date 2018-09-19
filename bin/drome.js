dafd6ce37fa41c88fc987138190ca0e91e13f144
kodipe <konrad.przydzial@gmail.com>
2018-09-19 14:05:32 +0200

    Add support for version printing in CLI (#15)

--- bin/drome.js
+++ bin/drome.js
@@ -3,14 +3,20 @@
 const path = require('path');
 const drome = require('../lib/drome.js');
 const { args } = require('../lib/cli');
+const { hasOption } = require('../lib/utils');
 let config;
+
+if(hasOption(process.argv, ['-V', '--version', 'version'])) {
+    console.log(`v${require('../package.json').version}`);
+    process.exit(0);
+}
 
 try {
     config = require(path.join(path.relative(__dirname, './'), 'drome.config.js'));
 } catch (e) {
     if (e.code == 'MODULE_NOT_FOUND') {
         console.log('There is no drome.config.js file in current directory');
-        process.exit();
+        process.exit(0);
     }
 }
 

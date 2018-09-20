8e44bacb951bcc81b8ec7d9dac3772ebeaf7bac5
kodipe <konrad.przydzial@gmail.com>
2018-09-21 00:04:22 +0200

    Create drome.config.js

--- drome.config.js
+++ drome.config.js
@@ -3,8 +3,8 @@ module.exports = () => {
         tasks: {
             install: 'npm install',
             test: {
-                unit: './node_modules/.bin/jest --color',
-                lint: './node_modules/.bin/eslint {bin,lib,test}/**/*.js'
+                unit: 'jest --color',
+                lint: 'eslint {bin,lib,test}/**/*.js'
             }
         }
     };

b93c6bf4d0b815fcbb6e0fc23f3157049d8cadfa
Mike Erickson <codedungeon@gmail.com>
2018-09-22 11:42:31 -0700

    wip

--- bin/drome.js
+++ bin/drome.js
@@ -1,18 +1,36 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const fs = require('fs');
+const homedir = require('os').homedir();
+
 const drome = require('../lib/drome.js');
 const { args } = require('../lib/cli');
-let config;
+const { colors } = require('../lib/colors');
 
-try {
-    config = require(path.join(path.relative(__dirname, './'), 'drome.config.js'));
-} catch (e) {
-    if (e.code == 'MODULE_NOT_FOUND') {
-        console.log('There is no drome.config.js file in current directory');
-        process.exit();
-    }
+let config;
+let homeConfig = () => { return { tasks: {} }; };
+let projectConfig = () => { return { tasks: {} }; };
+
+let homeConfigPath = path.join(homedir, 'drome.config.js');
+if (fs.existsSync(homeConfigPath)) {
+    homeConfig = require(homeConfigPath);
 }
+
+let projectConfigPath = path.join(path.relative(__dirname, './'), 'drome.config.js');
+// projectConfigPath = './drome.config.js'; // this is only used when debugging
+
+if (fs.existsSync(projectConfigPath)) {
+    projectConfig = require(projectConfigPath);
+} else {
+    console.log(colors.yellow('Unable to locate project `drome.config.js` only global tasks will be available.'));
+}
+
+config = () => {
+    return {
+        tasks: Object.assign(homeConfig().tasks, projectConfig().tasks)
+    };
+};
 
 drome(() => config(
     args(process.argv).rest
--- test/cli.test.js
+++ test/cli.test.js
@@ -11,3 +11,36 @@ test('cli.args return task and rest', () => {
     expect(result.task).toBe('task_name');
     expect(result.rest.length).toBe(2);
 });
+
+test('cli.args contains `--slient`', () => {
+    let result = args([
+        'node_path',
+        'origin_path',
+        'test_task',
+        '--silent',
+    ]);
+    expect(result.task).toBe('test_task');
+    expect(result.rest).toContain('--silent');
+});
+
+test('cli.args contains `-S`', () => {
+    let result = args([
+        'node_path',
+        'origin_path',
+        'test_task',
+        '-S',
+    ]);
+    expect(result.task).toBe('test_task');
+    expect(result.rest).toContain('-S');
+});
+
+test('cli.args contains `silent`', () => {
+    let result = args([
+        'node_path',
+        'origin_path',
+        'test_task',
+        'silent',
+    ]);
+    expect(result.task).toBe('test_task');
+    expect(result.rest).toContain('silent');
+});

7bbfc9a98ccb7c4d9280dc06d66e4fbc3326e744
kodipe <konrad.przydzial@gmail.com>
2018-09-24 13:57:55 +0200

    Exit code '1' on tasks failure (#2)

--- bin/drome.js
+++ bin/drome.js
@@ -16,10 +16,11 @@ try {
 } catch (e) {
     if (e.code == 'MODULE_NOT_FOUND') {
         console.log('There is no drome.config.js file in current directory');
-        process.exit(0);
+        process.exit(1);
     }
 }
 
 drome(() => config(
     args(process.argv).rest
-), args(process.argv).task, args(process.argv).rest);
+), args(process.argv).task, args(process.argv).rest)
+    .catch(() => process.exit(1));
